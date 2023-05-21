4f32c508871aed4afc1ac5e65dc99145f1ee20b9
Maksim Nemesh <user.email>
2023-05-21 20:31:21 +0300

    adding a branch with new code

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+        int quotient = firstNumber + secondNumber;
+        System.out.println("Сумма: " + quotient);
+        int quotient2 = firstNumber - secondNumber;
+        System.out.println("Разность: " + quotient2);
+        int quotient3 = firstNumber * secondNumber;
+        System.out.println("Произведение: " + quotient3);
+        double quotient4 = (double) firstNumber / secondNumber;
+        System.out.println("Частное: " + quotient4);
     }
 }
